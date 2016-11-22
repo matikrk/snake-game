0e62e978c5d338f9d9168e0987e742f752f1e4cf
matikrk <matikrk@google.com>
2016-11-22 11:00:37 +0100

    Refactor. Add canvas

--- src/main.js
+++ src/main.js
@@ -9,7 +9,8 @@ const getFi = function () {
 
 const board = {x: 400, y: 400};
 
-const rotationAngle= 0.07;
+const rotationAngle = 0.07;
+
 const rotateLeft = function () {
     fi -= rotationAngle;
 };
@@ -17,23 +18,42 @@ const rotateRight = function () {
     fi += rotationAngle;
 };
 
-const move = function (step = 1) {
+const calculateNextStep = function (x, y, step = 1) {
+    let newX = (x + step * Math.cos(getFi())) % board.x;
+    newX = newX <= 0 ? board.x - newX : newX;
+    let newY = (y + step * Math.sin(getFi())) % board.y;
+    newY = newY <= 0 ? board.y - newY : newY;
+
+    return {
+        x: newX,
+        y: newY,
+    }
+};
+
+const drawTailSVG = function () {
     const oldNode = svgElem.cloneNode(true);
     oldNode.removeAttribute('id');
     oldNode.classList.remove('snake-head');
     oldNode.classList.add('snake-tail');
     svgBoard.appendChild(oldNode);
-
-    const x = svgElem.getAttribute('x');
-    const y = svgElem.getAttribute('y');
-
-    let newX = (+x + step * Math.cos(getFi())) % board.x;
-    newX = newX <= 0 ? board.x - newX : newX;
-    let newY = (+y + step * Math.sin(getFi())) % board.y;
-    newY = newY <= 0 ? board.y - newY : newY;
-
-    svgElem.setAttribute('x', newX.toString());
-    svgElem.setAttribute('y', newY.toString());
 };
+
+const moveSVG = function (step = 1) {
+    drawTailSVG();
+
+    const oldX = svgElem.getAttribute('x');
+    const oldY = svgElem.getAttribute('y');
+
+    const {x, y} = calculateNextStep(+oldX, +oldY, step);
+
+
+    svgElem.setAttribute('x', x.toString());
+    svgElem.setAttribute('y', y.toString());
+};
+
+const move = function () {
+    moveSVG();
+};
+
 
 export default {move, rotateLeft, rotateRight};
